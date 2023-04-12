e4f7b24a05abb7762831f2420a8ed21fd0d53077
sojin <dlthwls0110@gmail.com>
2023-04-12 11:43:43 +0900

    commit

--- app/src/main/java/org/techtown/myproject/note/HeartReVAdapter.kt
+++ app/src/main/java/org/techtown/myproject/note/HeartReVAdapter.kt
@@ -3,6 +3,7 @@ package org.techtown.myproject.note
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -31,6 +32,10 @@ class HeartReVAdapter(val dogHeartList : ArrayList<DogHeartModel>):
         val myUid = FirebaseAuth.getInstance().currentUser?.uid.toString() // 현재 로그인된 유저의 uid
 
         holder.heartCntArea!!.text = dogHeartList[position].heartCount
+        if(dogHeartList[position].heartCount.toInt() > 30)
+            holder.state!!.setImageResource(R.drawable.arrow_up)
+        else
+            holder.state!!.setImageResource(R.drawable.minus)
 
         holder.itemView.setOnClickListener {
             itemClickListener.onClick(it, position)
@@ -45,5 +50,6 @@ class HeartReVAdapter(val dogHeartList : ArrayList<DogHeartModel>):
     inner class HeartViewHolder(view : View?) : RecyclerView.ViewHolder(view!!) {
         val view = view
         val heartCntArea = view?.findViewById<TextView>(R.id.heartCntArea)
+        val state = view?.findViewById<ImageView>(R.id.state)
     }
 }
