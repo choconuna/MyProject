dfd6a8ffe7dfd19352855b51394c0270472895b7
sojin <dlthwls0110@gmail.com>
2023-04-24 17:58:37 +0900

    commit

--- app/src/main/java/org/techtown/myproject/note/ImageDetailActivity.kt
+++ app/src/main/java/org/techtown/myproject/note/ImageDetailActivity.kt
@@ -3,12 +3,14 @@ package org.techtown.myproject.note
 import android.graphics.Bitmap
 import android.graphics.Point
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.Display
 import android.view.MotionEvent
 import android.view.ScaleGestureDetector
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import com.bumptech.glide.Glide
@@ -17,6 +19,8 @@ import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 import org.techtown.myproject.R
 import org.techtown.myproject.ZoomImageView
+import java.io.File
+import java.util.*
 import java.util.Collections.max
 import java.util.Collections.min
 
@@ -40,6 +44,30 @@ class ImageDetailActivity : AppCompatActivity() {
         imageView.debugInfoVisible = true
 
         getImage()
+
+        imageView.setOnLongClickListener {
+            // Firebase Storage에서 이미지 가져오기
+            val storageRef = Firebase.storage.reference
+            val imageRef = storageRef.child(image)
+
+            val localFile = File.createTempFile("image", "jpg")
+            imageRef.getFile(localFile)
+                .addOnSuccessListener {
+                    // 이미지 다운로드 완료 후 갤러리에 저장
+                    val savedImageUri = MediaStore.Images.Media.insertImage(contentResolver, localFile.absolutePath, UUID.randomUUID().toString(), "멍노트로부터 다운로드된 이미지")
+
+                    if (savedImageUri != null) {
+                        Toast.makeText(this, "이미지가 갤러리에 저장되었습니다.", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "이미지 저장에 실패하였습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "이미지 다운로드에 실패하였습니다.", Toast.LENGTH_SHORT).show()
+                }
+
+            true
+        }
 
         imageView.swipeToDismissEnabled = true
         imageView.onDismiss = {
